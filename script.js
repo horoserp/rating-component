35ea98e269fb9301f22bab55c09b12b2d56d7ae9
Robert Horosewski <horoserp@yahoo.com>
2024-12-07 10:44:02 -0500

    Update: Open thank you page on submit.

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const ratings = document.getElementsByClassName("number");
+const submitButton = document.getElementsByClassName("submit");
 
 for (let i = 0; i < ratings.length; i++) {
   ratings[i].addEventListener("click", () => {
-    var current = document.getElementsByClassName("active");
+    let current = document.getElementsByClassName("active");
     if (current[0]) {
       current[0].className = current[0].className.replace(" active", "");
     }
     ratings[i].className += " active";
   });
 }
+
+submitButton[0].addEventListener("click", () => {
+  window.location = "./thank-you.html";
+});
